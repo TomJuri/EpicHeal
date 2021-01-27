223b9e7cfba531a7302646086cca472277ef92b5
BuildTools <crxfterxd@gmail.com>
2021-01-27 18:36:30 +0100

    Added basic Project

---
+++ src/main/java/me/tomjuri/epicheal/EpicHeal.java
@@ -0,0 +1,13 @@
+package me.tomjuri.epicheal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EpicHeal extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+    public void onDisable() {
+
+    }
+}
